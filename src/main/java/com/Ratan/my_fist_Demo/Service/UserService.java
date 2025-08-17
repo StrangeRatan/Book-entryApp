d254378174e52a3db5b112088e8cc15deb0ea12b
Ratan <ratanyadav405@gmail.com>
2025-08-17 12:35:25 +0530

    Add Service package and Userservice inteface and there impletasion class

--- src/main/java/com/Ratan/my_fist_Demo/Service/UserService.java
+++ src/main/java/com/Ratan/my_fist_Demo/Service/UserService.java
@@ -1,4 +1,18 @@
 package com.Ratan.my_fist_Demo.Service;
 
-public class UserService {
+import com.Ratan.my_fist_Demo.DTO.Userdto;
+import com.Ratan.my_fist_Demo.Repository.UserRepository;
+import org.apache.catalina.User;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+public interface UserService {
+    Userdto createUser(Userdto userdto);
+    List<Userdto> getAllUsers();
+//    User getUserBy(Long id);
+//    User updateUser(Long id, Userdto userdto);
+//    void deleteUser(Long id);
+
 }
--- src/main/java/com/Ratan/my_fist_Demo/Service/impl/UserServiceImpl.java
+++ src/main/java/com/Ratan/my_fist_Demo/Service/impl/UserServiceImpl.java
@@ -2,24 +2,54 @@ package com.Ratan.my_fist_Demo.Service.impl;
 
 
 import com.Ratan.my_fist_Demo.DTO.Userdto;
+import com.Ratan.my_fist_Demo.Entity.UserEntity;
 import com.Ratan.my_fist_Demo.Repository.UserRepository;
-import org.apache.catalina.User;
+import com.Ratan.my_fist_Demo.Service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
 @Service  // This annotations   indicated this class is service class
-public class UserServicImpl {
+public class UserServiceImpl implements UserService {
 
-    private final UserRepository userRepository;
+    @Autowired
+    private  UserRepository userRepository;
 
-    public UserService (UserRepository userRepository){
-        this.userRepository=userRepository;
 
+
+    @Override
+    public Userdto createUser(Userdto userdto){
+        UserEntity userEntity= new UserEntity();
+        userEntity.setUsername(userdto.getUsername());
+        userEntity.setPassword(userdto.getPassword());
+
+        UserEntity savedUserEntity=userRepository.save(userEntity);
+
+        Userdto response=new Userdto();
+        response.setId(savedUserEntity.getId());
+        response.setUsername(savedUserEntity.getUsername());
+        response.setPassword(savedUserEntity.getPassword());
+        return response;
     }
 
     @Override
-    public User createUser(Userdto userdto){
-        User user= new User();
-        user.setUsername(userdto.getUsername());
-        user.setPassword(userdto.getPassword());
+    public List<Userdto> getAllUsers() {
+        return userRepository
+                .findAll()
+                .stream()
+                .map(user ->{
+                                 Userdto dto=new Userdto();
+                                 dto.setId(user.getId());
+                                 dto.setUsername(user.getUsername());
+                                 dto.setPassword(user.getPassword());
+                                 return dto;
+                })
+                        .collect(Collectors.toList());
+
     }
 }
+
+
