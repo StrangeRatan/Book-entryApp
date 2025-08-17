6e64cee6e3fde1e57ed8e8c82a7a27cb9bc8b2a6
Ratan <ratanyadav405@gmail.com>
2025-08-17 11:52:51 +0530

    Add Dto package and class

---
+++ src/main/java/com/Ratan/my_fist_Demo/DTO/Userdto.java
@@ -0,0 +1,23 @@
+package com.Ratan.my_fist_Demo.DTO;
+
+public class Userdto {
+    private String username;
+
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ src/main/java/com/Ratan/my_fist_Demo/Service/UserService.java
@@ -0,0 +1,4 @@
+package com.Ratan.my_fist_Demo.Service;
+
+public class UserService {
+}
