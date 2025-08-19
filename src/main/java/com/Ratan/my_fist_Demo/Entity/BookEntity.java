c49b1af06ff9c7de6b5d2d3399437303af596b62
Ratan <ratanyadav405@gmail.com>
2025-08-19 15:35:18 +0530

    Create BookEntity

---
+++ src/main/java/com/Ratan/my_fist_Demo/Entity/BookEntity.java
@@ -0,0 +1,57 @@
+package com.Ratan.my_fist_Demo.Entity;
+
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import org.antlr.v4.runtime.misc.NotNull;
+import org.springframework.lang.NonNull;
+
+import java.time.LocalDateTime;
+
+@Table(name="Book")
+public class BookEntity {
+
+    @Id
+    private Long serialNo;
+
+    @NonNull
+    private String authorname;
+
+    @NonNull
+    private String booktitle;
+
+    private LocalDateTime date;
+
+    public Long getSerialNo() {
+        return serialNo;
+    }
+
+    public void setSerialNo(Long serialNo) {
+        this.serialNo = serialNo;
+    }
+
+    @NonNull
+    public String getAuthorname() {
+        return authorname;
+    }
+
+    public void setAuthorname(@NonNull String authorname) {
+        this.authorname = authorname;
+    }
+
+    @NonNull
+    public String getBooktitle() {
+        return booktitle;
+    }
+
+    public void setBooktitle(@NonNull String booktitle) {
+        this.booktitle = booktitle;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+}
